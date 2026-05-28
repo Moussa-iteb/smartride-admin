ffdeab052685d02e299f0d90adc64300c0e51b07
Moussa-iteb <itebmoussa577@gmail.com>
2026-05-28 15:27:19 +0200

    update environments with apiUrl and firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://application-production-4e3f.up.railway.app',
+  firebaseConfig: {
+    apiKey: 'AIzaSyA-mVLD4A_ZX4cdTqm--IA1CpsJgXnIqv4',
+    authDomain: 'smartride-7e6ee.firebaseapp.com',
+    projectId: 'smartride-7e6ee',
+    storageBucket: 'smartride-7e6ee.firebasestorage.app',
+    messagingSenderId: '28187051774',
+    appId: '1:28187051774:web:040f0e25aca73af25e87af',
+    measurementId: 'G-3K6B1B5958'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  
+  apiUrl: 'http://localhost:3000',
   firebaseConfig: {
     apiKey: 'AIzaSyA-mVLD4A_ZX4cdTqm--IA1CpsJgXnIqv4',
     authDomain: 'smartride-7e6ee.firebaseapp.com',
@@ -10,5 +10,4 @@ export const environment = {
     appId: '1:28187051774:web:040f0e25aca73af25e87af',
     measurementId: 'G-3K6B1B5958'
   }
-  
 };
